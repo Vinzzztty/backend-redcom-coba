964729ae9fff0d62dd4d265857c91053c79b331b
Vinzzztty <kevinarnandes21@gmail.com>
2023-07-06 15:24:56 +0700

    fix: change format time-zone

--- server/utils/formattedDate.js
+++ server/utils/formattedDate.js
@@ -1,19 +1,25 @@
 const formatDate = (dateString) => {
     const date = new Date(Date.parse(dateString));
-    return date.toLocaleDateString("id-ID", {
+    const options = {
         day: "numeric",
         month: "long",
         year: "numeric",
-    });
+        timeZone: "Asia/Bangkok", // Set the time zone to Bangkok
+    };
+
+    return date.toLocaleDateString("en-US", options);
 };
 
 // Format the time as "HH:mm" (e.g., 13:25)
 const formatTime = (dateString) => {
     const date = new Date(Date.parse(dateString));
-    return date.toLocaleTimeString("id-ID", {
+    const options = {
         hour: "2-digit",
         minute: "2-digit",
-    });
+        timeZone: "Asia/Bangkok", // Set the time zone to Bangkok
+    };
+
+    return date.toLocaleTimeString("en-US", options);
 };
 
 module.exports = { formatDate, formatTime };
