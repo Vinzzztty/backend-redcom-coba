26c0e0764e809b805678ff5130aa67607b991aae
Vinzzztty <kevinarnandes21@gmail.com>
2023-07-06 14:03:24 +0700

    chore: make column kategori_log

--- server/models/Kategori.js
+++ server/models/Kategori.js
@@ -4,6 +4,9 @@ const kategoriSchema = new mongoose.Schema({
     kategori: {
         type: String,
     },
+    kategori_logo: {
+        type: String,
+    },
 });
 
 const Kategori = mongoose.model("Kategori", kategoriSchema);
