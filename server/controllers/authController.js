669f87e34a3d3bf0706daa3f2e314a3fcb4fe87f
Vinzzztty <kevinarnandes21@gmail.com>
2023-07-06 19:15:07 +0700

    fix: fix total_comments

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -175,22 +175,20 @@ exports.getUserPost = async (req, res) => {
                 });
             }
 
+            const totalComments = await Comment.countDocuments({
+                user_id: userId,
+            });
+
             const formattedPosts = await Promise.all(
                 posts.map(async (post) => {
                     const formattedCreatedAtDate = formatDate(post.crdAt);
                     const formattedCreatedAtTime = formatTime(post.crdAt);
-
-                    const totalComments = await Comment.countDocuments({
-                        post_id: post._id,
-                        user_id: userId,
-                    });
 
                     return {
                         _id: post._id,
                         content: post.content,
                         kategori_id: post.kategori_id,
                         user_id: post.user_id,
-                        total_comments: totalComments,
                         date_created: formattedCreatedAtDate,
                         time: formattedCreatedAtTime,
                     };
@@ -199,7 +197,7 @@ exports.getUserPost = async (req, res) => {
 
             res.status(200).json({
                 status: "success",
-                data: formattedPosts,
+                data: { formattedPosts, totalComments },
             });
         });
     } catch (error) {
